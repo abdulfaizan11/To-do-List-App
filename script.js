7e28295358d13122146f5b0f91884a69d1e6e400
abdulfaizan11 <162877452+abdulfaizan11@users.noreply.github.com>
2024-03-17 15:00:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box")
+const listConatiner = document.getElementById("list-container")
+
+function addItem(){
+    if(inputBox.value === ''){
+        alert("write something in a box")
+    }
+    else{
+        let li = document.createElement("li")
+        li.innerHTML=inputBox.value
+        listConatiner.appendChild(li)
+
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+
+    }
+    inputBox.value=''
+    saveData()
+}
+
+listConatiner.addEventListener("click" , (e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    }
+    else if(e.target.tagName=="SPAN"){
+        e.target.parentElement.remove()
+        saveData()
+    }
+},false)
+
+
+function saveData(){
+    localStorage.setItem("data", listConatiner.innerHTML)
+
+}
+function showTask(){
+    listConatiner.innerHTML= localStorage.getItem("data")
+}
+showTask()
+
+
+
+
